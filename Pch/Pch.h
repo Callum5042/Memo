f208f99e32a8db3c84695f209475d117f7b04180
Callum Anning <CallumAnning@hotmail.co.uk>
2021-12-17 20:10:35 +0000

    Added precompiled headers project

---
+++ Pch/Pch.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+#include <memory>
+#include <vector>
+
+#if defined(WIN32) || defined(_WIN32)
+#include <Windows.h>
+#pragma comment(linker,"\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+#endif
