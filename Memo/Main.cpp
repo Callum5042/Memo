fb9b100707be02fa3e212495ac0692bcf9feae19
Callum Anning <CallumAnning@hotmail.co.uk>
2021-12-19 17:27:46 +0000

    Added WPF project

--- Memo/Main.cpp
+++ Memo/Main.cpp
@@ -49,13 +49,13 @@ public:
 		AddButton(MTK::ButtonCreateSettings{ L"Click me tooo", 10, 50, 100, 20, 2 });*/
 
 		// Add menu
-		/*HMENU menu = CreateMenu();
+		HMENU menu = CreateMenu();
 		HMENU fileMenu = CreateMenu();
 
 		AppendMenu(menu, MF_POPUP, reinterpret_cast<UINT_PTR>(fileMenu), L"File");
-		AppendMenu(fileMenu, MF_STRING, 1, L"Open");
+		AppendMenu(fileMenu, MF_STRING, 10, L"Open");
 
-		SetMenu(m_Hwnd, menu);*/
+		SetMenu(m_Hwnd, menu);
 	}
 };
 
---
+++ MemoWpf/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Memo.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_NewTab_Click(object sender, RoutedEventArgs e)
+        {
+            // Add new tab and auto switch to that tab
+            Tabs.Items.Add(new TabItem()
+            {
+                Header = "New Tab",
+                IsSelected = true
+            });
+
+
+        }
+
+        private void TabItem_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var menu = new Menu();
+
+        }
+    }
+}
