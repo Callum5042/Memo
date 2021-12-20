b9a575ba02baf6bd7c2a928bf6082a888b63fd73
Callum Anning <CallumAnning@hotmail.co.uk>
2021-12-20 12:11:15 +0000

    Added window pop out

--- MemoWpf/MainWindow.xaml.cs
+++ MemoWpf/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -15,6 +17,24 @@ using System.Windows.Shapes;
 
 namespace Memo.WPF
 {
+    public static class WindowsServices
+    {
+        const int WS_EX_TRANSPARENT = 0x00000020;
+        const int GWL_EXSTYLE = (-20);
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowLong(IntPtr hwnd, int index);
+
+        [DllImport("user32.dll")]
+        static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
+
+        public static void SetWindowExTransparent(IntPtr hwnd)
+        {
+            var extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT);
+        }
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -33,6 +53,50 @@ namespace Memo.WPF
             //    Header = "New Tab",
             //    IsSelected = true
             //});
+        }
+
+        private void MenuItem_PopOut_Click(object sender, RoutedEventArgs e)
+        {
+            var window = new Window
+            {
+                ResizeMode = ResizeMode.CanResizeWithGrip,
+                WindowStyle = WindowStyle.None,
+                Topmost = true,
+                Width = 200,
+                Height = 100,
+                Background = new SolidColorBrush(Colors.White),
+                BorderBrush = new SolidColorBrush(Colors.LightGray),
+                AllowsTransparency = true,
+                ShowInTaskbar = false
+            };
+
+            window.BorderThickness = new Thickness(1.0);
+            //window.Background.Opacity = 0.5;
+
+            // Enable mouse events to pass through the window
+            //window.SourceInitialized += (sender, e) =>
+            //{
+            //    var hwnd = new WindowInteropHelper(window).Handle;
+            //    WindowsServices.SetWindowExTransparent(hwnd);
+            //};
+            
+            // Allow window to be dragged anywhere
+            window.MouseDown += (sender, e) =>
+            {
+                if (e.ChangedButton == MouseButton.Left)
+                {
+                    window.DragMove();
+                }
+            };
+
+            // Add text
+            var text = new TextBlock();
+            text.Text = "Hello, World!";
+            text.Padding = new Thickness(5.0, 0.0, 5.0, 0.0);
+
+            window.Content = text;
+
+            window.Show();
         }
     }
 }
