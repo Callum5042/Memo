d537bb4fd11729b223b2c1435f71ffd7332352b4
Callum Anning <CallumAnning@hotmail.co.uk>
2022-01-02 22:26:24 +0000

    Direct2D

--- Memo/Main.cpp
+++ Memo/Main.cpp
@@ -4,6 +4,9 @@
 #include <Windows.h>
 #endif
 
+#include <d2d1.h>
+#pragma comment(lib, "d2d1")
+
 template <class TWindow>
 class BaseWindow
 {
@@ -89,29 +92,125 @@ protected:
 
 class Window : public BaseWindow<Window>
 {
+	ID2D1Factory* pFactory;
+	ID2D1HwndRenderTarget* pRenderTarget;
+	ID2D1SolidColorBrush* pBrush;
+	D2D1_ELLIPSE            ellipse;
+
 public:
 	Window() = default;
 	virtual ~Window() = default;
+
+	void CalculateLayout()
+	{
+		if (pRenderTarget != NULL)
+		{
+			D2D1_SIZE_F size = pRenderTarget->GetSize();
+			const float x = size.width / 2;
+			const float y = size.height / 2;
+			const float radius = min(x, y);
+			ellipse = D2D1::Ellipse(D2D1::Point2F(x, y), radius, radius);
+		}
+	}
+
+	HRESULT CreateGraphicsResources()
+	{
+		HRESULT hr = S_OK;
+		if (pRenderTarget == NULL)
+		{
+			RECT rc;
+			GetClientRect(m_Hwnd, &rc);
+
+			D2D1_SIZE_U size = D2D1::SizeU(rc.right, rc.bottom);
+
+			hr = pFactory->CreateHwndRenderTarget(
+				D2D1::RenderTargetProperties(),
+				D2D1::HwndRenderTargetProperties(m_Hwnd, size),
+				&pRenderTarget);
+
+			if (SUCCEEDED(hr))
+			{
+				const D2D1_COLOR_F color = D2D1::ColorF(1.0f, 1.0f, 0);
+				hr = pRenderTarget->CreateSolidColorBrush(color, &pBrush);
+
+				if (SUCCEEDED(hr))
+				{
+					CalculateLayout();
+				}
+			}
+		}
+		return hr;
+	}
+
+	void DiscardGraphicsResources()
+	{
+		//SafeRelease(&pRenderTarget);
+		//SafeRelease(&pBrush);
+	}
+
+	void OnPaint()
+	{
+		HRESULT hr = CreateGraphicsResources();
+		if (SUCCEEDED(hr))
+		{
+			PAINTSTRUCT ps;
+			BeginPaint(m_Hwnd, &ps);
+
+			pRenderTarget->BeginDraw();
+
+			pRenderTarget->Clear(D2D1::ColorF(D2D1::ColorF::SkyBlue));
+			pRenderTarget->FillEllipse(ellipse, pBrush);
+
+			hr = pRenderTarget->EndDraw();
+			if (FAILED(hr) || hr == D2DERR_RECREATE_TARGET)
+			{
+				DiscardGraphicsResources();
+			}
+
+			EndPaint(m_Hwnd, &ps);
+		}
+	}
+
+	void Resize()
+	{
+		if (pRenderTarget != NULL)
+		{
+			RECT rc;
+			GetClientRect(m_Hwnd, &rc);
+
+			D2D1_SIZE_U size = D2D1::SizeU(rc.right, rc.bottom);
+
+			pRenderTarget->Resize(size);
+			CalculateLayout();
+			InvalidateRect(m_Hwnd, NULL, FALSE);
+		}
+	}
 
 public:
 	LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) override
 	{
 		switch (uMsg)
 		{
+		case WM_CREATE:
+			if (FAILED(D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &pFactory)))
+			{
+				return -1;  // Fail CreateWindowEx.
+			}
+			return 0;
+
 		case WM_DESTROY:
 			PostQuitMessage(0);
 			return 0;
 
 		case WM_PAINT:
 		{
-			PAINTSTRUCT ps;
-			HDC hdc = BeginPaint(m_Hwnd, &ps);
-
-			FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-
-			EndPaint(m_Hwnd, &ps);
+			OnPaint();
 			return 0;
 		}
+
+		case WM_SIZE:
+			Resize();
+			return 0;
 
 		case WM_CLOSE:
 			if (MessageBox(m_Hwnd, L"Are you sure you want to quit?", L"Oh no", MB_OKCANCEL) == IDOK)
